2ba4553dc7c5edbe0e2987b2eff169a4201ca2a9
Manuel Achenapure <machenapure@gmail.com>
2021-01-25 23:40:14 -0300

    First delivery of the final project. Cards are working well, there are some css details that must be fixed. JS working well.

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-console.log(`Funciona`)
+class Product{
+    
+    constructor(name, price, stock, img){
+        this.name=name;
+        this.price=price;
+        this.stock=stock;
+        this.img=img;
+    }
+
+    printCard(){
+        let newNode = document.createElement('div');
+        newNode.className = 'card'
+        newNode.innerHTML = `
+        <img src="${this.img}">
+        <h4>${this.name}</h4>
+        <p>Precio: $${this.price}</p>
+        <a href="#">Comprar</a>
+        `
+        document.getElementById("containerCard").appendChild(newNode); 
+    }
+}
+
+let dataBase = [];
+
+let zapNikeAirMot2 = new Product('Zapatillas Nike Airmax Motion 2', '9299', '10', 'imgs/nike-airmax-motion2.jpg');
+let buzoPumaClass = new Product('Buzo Puma Classic', '5499', '15', 'imgs/buzo-puma-classic.jpg');
+let zapAdiOrigRunRosa = new Product('Zapatillas Adidas Originals Run', '7499', '5', 'imgs/adidas-originals-run.jpg')
+
+dataBase[0] = zapNikeAirMot2;
+dataBase[1] = buzoPumaClass;
+dataBase[2] = zapAdiOrigRunRosa;
+
+for(let i=0; i<dataBase.length; i++){
+    dataBase[i].printCard();
+}
